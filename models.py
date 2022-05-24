82c8881ec7d9bb968692e891ff2fb410a6ff8133
mn9142001 <75080962+mn9142001@users.noreply.github.com>
2022-05-24 08:43:32 +0200

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Union
-
-
-class UserModel(BaseModel):
-    id : int
-    name: str
-    img : str = "https://png.pngtree.com/png-clipart/20190520/original/pngtree-vector-users-icon-png-image_4144740.jpg"
