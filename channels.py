7825fab26f5b0ac3e26fb28d760a8ec7fc730925
mn9142001 <75080962+mn9142001@users.noreply.github.com>
2022-05-27 06:52:05 +0200

    Update channels.py

--- channels.py
+++ channels.py
@@ -18,13 +18,12 @@ class BaseLayer:
             data = json.loads(data)
         except:
             raise "Data must be of dict type"
-
         await self.receive(_group, data)
 
     async def group_send_and_exc(self, groupName, channelName, data):
         data = data
         group = self.get_groups().get(groupName, {})
-        for c in group:
+        for c in group.values():
             if not c == channelName:
                 await group[c].send_text(data)
 
@@ -33,34 +32,33 @@ class BaseLayer:
         assert ChannelName in group.keys(), "No Channel with that name exists"
         await group[ChannelName].send_text(data)
 
-
 class InMemoryManager(BaseLayer):
     groups = {}
-    async def group_add(self, _group, channel, channelName:str=None):
+    async def group_add(self, _group, channel, channelName:str = None):
         if channelName and not channelName.isidentifier():
             raise "Channel names must be an identifer"
-            
         group = self.get_groups().setdefault(_group, {})
         name = channelName if channelName else await RandomChannelName()
         group.update({name : channel})
-        await channel.accept()
+        await self.on_connect(_group, channel)
+
         try:
             while True:
-                await self.on_connect(_group, channel)
                 data = await channel.receive_text()
                 await self._receive(_group, data)
                 
-        except WebSocketDisconnect:
-            await self.disconnect(_group, channel)
+        except WebSocketDisconnect:            
+            await self.disconnect(_group, name)
 
-    async def group_discard(self, groupName, channel):
+
+    async def group_discard(self, groupName, channelName):
         group = self.get_groups().get(groupName, False)
         if group:
-            if channel in group:
-                group.remove(channel)
+            if group.get(channelName, False):
+                del group[channelName]
                 if not group:
-                    self.get_groups().pop(groupName)
-    
+                    del self.get_groups()[groupName]
+
     def get_groups(self):
         return self.groups
 
@@ -68,19 +66,20 @@ ChannlsManager = os.environ.get('FASTAPI_SOCKET_MANAGER', InMemoryManager)
 
 class SocketManager(ChannlsManager):
     #on connection, you can close connection with await channel.close()
-    async def on_connect(self, groupName, channel):
-        pass
+    async def on_connect(self, group_name, channel):
+        await channel.accept()
 
     #disconnect
-    async def disconnect(self, _group, channel):
-        await self.group_discard(_group, channel)
+    async def disconnect(self, group_name, channelName):
+        await self.group_discard(group_name, channelName)
 
     #receive from websocket
-    async def receive(self, name, data):
-        await self.group_send(name, json.dumps(data))
+    async def receive(self, group_name, data):
+        await self.group_send(group_name, json.dumps(data))
 
     #receive message from server and send to websocket
     async def group_send(self, group_name, data):
+        
         await self.send(group_name, data)
 
 FastSocket = SocketManager()
